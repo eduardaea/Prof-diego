eeb90a2604f408a340a6b517ade9aefc05f3169a
Eduarda <duda.eduarda.ec@gmail.com>
2022-10-05 17:37:29 -0300

    Criação da estilização do Inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'inicio', component: InicioComponent}
+
 ];
 
 @NgModule({
